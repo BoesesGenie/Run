76d4b2b0e9127640d5c569fc5f83837ce75947f9
Konstantin Kharlamov <k.kharlamow@gmail.com>
2026-05-23 17:40:08 +0500

    Factory header

---
+++ Factory.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <vector>
+#include <SFML/Graphics.hpp>
+#include "GameObject.h"
+
+using namespace sf;
+
+class Factory
+{
+private:
+	RenderWindow* m_Window;
+public:
+	Factory(RenderWindow* window);
+	void loadLevel(
+		std::vector<GameObject*>& gameObjects,
+		VertexArray& canvas,
+		InputDispatcher& inputDispatcher
+	);
+	Texture* m_Texture;
+};
