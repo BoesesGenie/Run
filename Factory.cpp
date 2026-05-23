663f05536c9e72c8dea993e088610b18e03caf18
Konstantin Kharlamov <k.kharlamow@gmail.com>
2026-05-23 17:51:12 +0500

    Factory implementation, media added

--- Factory.cpp
+++ Factory.cpp
@@ -1,1 +1,22 @@
 #include "Factory.h"
+#include <iostream>
+
+Factory::Factory(RenderWindow* window)
+{
+	m_Window = window;
+	m_Texture = new Texture();
+	
+	if (!m_Texture->loadFromFile("graphics/texture.png"))
+	{
+		std::cerr << "Failed to load texture" << std::endl;
+	}
+}
+
+void Factory::loadLevel(
+	std::vector<GameObject*>& gameObjects,
+	VertexArray& canvas,
+	InputDispatcher& inputDispatcher
+)
+{
+	// TODO
+}
